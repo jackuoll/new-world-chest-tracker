056b99fb86b43d2cf862dd6b4f718110c4687dbd
Jack Ward <jackward84@gmail.com>
2021-12-04 18:05:22 +1300

    include some data

---
+++ data/__init__.py
@@ -0,0 +1,198 @@
+reekwater = "Reekwater"
+shattered = "Shattered"
+weavers = "Weaver's Fen"
+md = "Mourningdale"
+
+chest_alias_list = {
+  "614f0330519d9fd5eba645ee": {
+    "zone": "Reekwater",
+    "name": "Village 1"
+  },
+  "614f0330519d9fd5eba645ef": {
+    "zone": "Reekwater",
+    "name": "Village 2"
+  },
+  "614f0330519d9fd5eba645ed": {
+    "zone": "Reekwater",
+    "name": "Village 3"
+  },
+  "614f0330519d9fd5eba645f4": {
+    "zone": "Reekwater",
+    "name": "Virid Grotto Ledge"
+  },
+  "614f0330519d9fd5eba645f3": {
+    "zone": "Reekwater",
+    "name": "Single Cave"
+  },
+  "614f0330519d9fd5eba645e1": {
+    "zone": "Reekwater",
+    "name": "Swamp"
+  },
+  "614f0330519d9fd5eba645e0": {
+    "zone": "Reekwater",
+    "name": "Small Cave"
+  },
+  "614f0330519d9fd5eba645f0": {
+    "zone": "Reekwater",
+    "name": "Right Entrance"
+  },
+  "614f0330519d9fd5eba645f2": {
+    "zone": "Reekwater",
+    "name": "Tentacle"
+  },
+  "614f0330519d9fd5eba645f1": {
+    "zone": "Reekwater",
+    "name": "Deep"
+  },
+  "614f0331519d9fd5eba645f8": {
+    "zone": "Reekwater",
+    "name": "Boss"
+  },
+  "614f032e519d9fd5eba645c4": {
+    "zone": "Shattered",
+    "name": " Mines"
+  },
+  "614f0330519d9fd5eba645e6": {
+    "zone": "Reekwater",
+    "name": "Tower 1"
+  },
+"614f0330519d9fd5eba645e7": {
+    "zone": "Reekwater",
+    "name": "Tower 2"
+  },
+  "614f0330519d9fd5eba645e4": {
+    "zone": "Reekwater",
+    "name": "Tower 3"
+  },
+  "614f0330519d9fd5eba645e9": {
+    "zone": "Reekwater",
+    "name": "Tower 4"
+  },
+  "614f0330519d9fd5eba645e5": {
+    "zone": "Reekwater",
+    "name": "Tower 5",
+    "unreachable": True
+  },
+"614f0330519d9fd5eba645eb": {
+    "zone": "Reekwater",
+    "name": "Tower 6",
+    "unreachable": True
+  },
+  "614f0330519d9fd5eba645ea": {
+    "zone": "Reekwater",
+    "name": "Surgeon"
+  },
+  "614f0330519d9fd5eba645ec": {
+    "zone": "Reekwater",
+    "name": "Ancients Pit"
+  },
+  "614f0330519d9fd5eba645e8": {
+    "zone": "Reekwater",
+    "name": "Ancients Platform"
+  },
+  "614f0334519d9fd5eba6465a": {
+    "zone": "Weaver's Fen",
+    "name": "Waterfall"
+  },
+  "614f0331519d9fd5eba64605": {
+    "zone": "Weaver's Fen",
+    "name": "Church"
+  },
+  "614f0331519d9fd5eba64606": {
+    "zone": "Weaver's Fen",
+    "name": "House 1"
+  },
+  "614f0332519d9fd5eba6461f": {
+    "zone": "Weaver's Fen",
+    "name": "House 2"
+  },
+  "614f0333519d9fd5eba64636": {
+    "zone": "Weaver's Fen",
+    "name": "Field"
+  },
+  "614f0333519d9fd5eba6463a": {
+    "zone": "Weaver's Fen",
+    "name": "Village 1"
+  },
+  "614f0333519d9fd5eba6463b": {
+    "zone": "Weaver's Fen",
+    "name": "Village 2"
+  },
+  "614f0333519d9fd5eba6463d": {
+    "zone": "Weaver's Fen",
+    "name": "Village 3"
+  },
+  "614f0333519d9fd5eba64635": {
+    "zone": "Weaver's Fen",
+    "name": "Village 4"
+  },
+  "614f0333519d9fd5eba6463c": {
+    "zone": "Weaver's Fen",
+    "name": "Village 5"
+  },
+  "614f0337519d9fd5eba6469c": {
+    "zone": "Mourningdale",
+    "name": "Haetus"
+  },
+  "614f0338519d9fd5eba646a8": {
+    "zone": "Mourningdale",
+    "name": "Vela"
+  },
+  "614f0337519d9fd5eba64698": {
+    "zone": "Mourningdale",
+    "name": "Crux"
+  },
+  "614f0337519d9fd5eba64694": {
+    "zone": "Mourningdale",
+    "name": "Echowhine Hollow"
+  },
+  "614f0337519d9fd5eba6469f": {
+    "zone": "Mourningdale",
+    "name": "Lesath"
+  },
+  "614f0337519d9fd5eba646a1": {
+    "zone": "Mourningdale",
+    "name": "Swamp"
+  },
+  "614f0337519d9fd5eba646a0": {
+    "zone": "Mourningdale",
+    "name": "Propus 1"
+  },
+  "614f0337519d9fd5eba64699": {
+    "zone": "Mourningdale",
+    "name": "Propus 2"
+  },
+  "614f0337519d9fd5eba6469a": {
+    "zone": "Mourningdale",
+    "name": "Propus 3"
+  },
+  "614f0337519d9fd5eba64697": {
+    "zone": "Mourningdale",
+    "name": "Tormglade"
+  },
+  "614f0336519d9fd5eba64687": {
+    "zone": "Mourningdale",
+    "name": "Nembus"
+  },
+  "614f0338519d9fd5eba646a9": {
+    "zone": "Mourningdale",
+    "name": "Vela 2"
+  },
+  "614f0336519d9fd5eba64688": {
+    "zone": "Mourningdale",
+    "name": "Guerra's Curse"
+  },
+  "614f0337519d9fd5eba64696": {
+    "zone": "Mourningdale",
+    "name": "Stormglade"
+  },
+  "6187470f3059b94f5bd99987": {
+    "zone": "War Board",
+    "name": "War Board"
+  },
+  "614f0337519d9fd5eba64695": {
+    "zone": "Mourningdale",
+    "name": "Wolf Cave"
+  }
+}
+
